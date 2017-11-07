7c9e3cecc22a264771d88f445f5da69c911fafd2
oktauploader-okta <oktauploader@okta.com>
2017-11-07 10:47:34 -0800

    Initial commit

---
+++ src/main/groovy/com/okta/test/mock/scenarios/OIDCCodeLocalValidationScenarioDefinition.groovy
@@ -0,0 +1,327 @@
+/*
+ * Copyright 2017 Okta, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.okta.test.mock.scenarios
+
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.okta.test.mock.wiremock.TestUtils
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import org.apache.commons.codec.binary.Base64
+
+import java.security.KeyPair
+import java.security.KeyPairGenerator
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+import java.util.regex.Pattern
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse
+import static com.github.tomakehurst.wiremock.client.WireMock.containing
+import static com.github.tomakehurst.wiremock.client.WireMock.get
+import static com.github.tomakehurst.wiremock.client.WireMock.matching
+import static com.github.tomakehurst.wiremock.client.WireMock.post
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+
+class OIDCCodeLocalValidationScenarioDefinition implements ScenarioDefinition {
+
+    String pubKeyE
+    String pubKeyN
+    String accessTokenJwt
+    String idTokenJwt
+    String wrongKeyIdIdTokenJwt
+    String wrongAudienceIdTokenJwt
+    String issuedInFutureIdTokenJwt
+    String expiredIdTokenJwt
+    String invalidSignatureIdTokenJwt
+    String invalidIssuerIdTokenJwt
+    String invalidNotBeforeIdTokenJwt
+    String clientId = "OOICU812"
+    String clientSecret = "VERY_SECRET"
+    String authHeader = "Basic " + "${clientId}:${clientSecret}".bytes.encodeBase64().toString()
+
+    OIDCCodeLocalValidationScenarioDefinition() {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA")
+        keyPairGenerator.initialize(4096)
+        KeyPair invalidKeyPair = keyPairGenerator.generateKeyPair()
+        KeyPair keyPair = keyPairGenerator.generateKeyPair()
+
+        pubKeyE = Base64.encodeBase64URLSafeString(TestUtils.toIntegerBytes(keyPair.publicKey.getPublicExponent()))
+        pubKeyN = Base64.encodeBase64URLSafeString(TestUtils.toIntegerBytes(keyPair.publicKey.getModulus()))
+
+        Instant now = Instant.now()
+        accessTokenJwt =  Jwts.builder()
+                .setSubject("joe.coder@example.com")
+                .setAudience("api://default")
+                .claim("scp", ["profile", "openid", "email"])
+                .setIssuedAt(Date.from(now))
+                .setNotBefore(Date.from(now))
+                .setExpiration(Date.from(now.plus(1, ChronoUnit.HOURS)))
+                .setHeader(Jwts.jwsHeader()
+                .setKeyId('TEST_PUB_KEY_ID'))
+                .signWith(SignatureAlgorithm.RS256, keyPair.privateKey)
+                .compact()
+
+        idTokenJwt =  Jwts.builder()
+                .setSubject("00uid4BxXw6I6TV4m0g3")
+                .claim("name", "Joe Coder")
+                .claim("email", "joe.coder@example.com")
+                .claim("preferred_username", "jod.coder@example.com")
+                .setAudience("OOICU812")
+                .setIssuer("http://localhost:9090/oauth2/default")
+                .setIssuedAt(Date.from(now))
+                .setNotBefore(Date.from(now))
+                .setExpiration(Date.from(now.plus(1, ChronoUnit.HOURS)))
+                .setHeader(Jwts.jwsHeader()
+                .setKeyId('TEST_PUB_KEY_ID'))
+                .signWith(SignatureAlgorithm.RS256, keyPair.privateKey)
+                .compact()
+
+        wrongKeyIdIdTokenJwt =  Jwts.builder()
+                .setSubject("00uid4BxXw6I6TV4m0g3")
+                .claim("name", "Joe Coder")
+                .claim("email", "joe.coder@example.com")
+                .claim("preferred_username", "jod.coder@example.com")
+                .setAudience("OOICU812")
+                .setIssuer("http://localhost:9090/oauth2/default")
+                .setIssuedAt(Date.from(now))
+                .setNotBefore(Date.from(now))
+                .setExpiration(Date.from(now.plus(1, ChronoUnit.HOURS)))
+                .setHeader(Jwts.jwsHeader()
+                .setKeyId('WRONG_TEST_PUB_KEY_ID'))
+                .signWith(SignatureAlgorithm.RS256, keyPair.privateKey)
+                .compact()
+
+        wrongAudienceIdTokenJwt =  Jwts.builder()
+                .setSubject("00uid4BxXw6I6TV4m0g3")
+                .claim("name", "Joe Coder")
+                .claim("email", "joe.coder@example.com")
+                .claim("preferred_username", "jod.coder@example.com")
+                .setAudience("WRONG_OOICU812")
+                .setIssuer("http://localhost:9090/oauth2/default")
+                .setIssuedAt(Date.from(now))
+                .setNotBefore(Date.from(now))
+                .setExpiration(Date.from(now.plus(1, ChronoUnit.HOURS)))
+                .setHeader(Jwts.jwsHeader()
+                .setKeyId('TEST_PUB_KEY_ID'))
+                .signWith(SignatureAlgorithm.RS256, keyPair.privateKey)
+                .compact()
+
+        issuedInFutureIdTokenJwt =  Jwts.builder()
+                .setSubject("00uid4BxXw6I6TV4m0g3")
+                .claim("name", "Joe Coder")
+                .claim("email", "joe.coder@example.com")
+                .claim("preferred_username", "jod.coder@example.com")
+                .setAudience("OOICU812")
+                .setIssuer("http://localhost:9090/oauth2/default")
+                .setIssuedAt(Date.from(now.plus(1, ChronoUnit.HOURS)))
+                .setNotBefore(Date.from(now))
+                .setExpiration(Date.from(now.plus(1, ChronoUnit.HOURS)))
+                .setHeader(Jwts.jwsHeader()
+                .setKeyId('TEST_PUB_KEY_ID'))
+                .signWith(SignatureAlgorithm.RS256, keyPair.privateKey)
+                .compact()
+
+        expiredIdTokenJwt =  Jwts.builder()
+                .setSubject("00uid4BxXw6I6TV4m0g3")
+                .claim("name", "Joe Coder")
+                .claim("email", "joe.coder@example.com")
+                .claim("preferred_username", "jod.coder@example.com")
+                .setAudience("OOICU812")
+                .setIssuer("http://localhost:9090/oauth2/default")
+                .setIssuedAt(Date.from(now))
+                .setNotBefore(Date.from(now))
+                .setExpiration(Date.from(now.minus(1, ChronoUnit.HOURS)))
+                .setHeader(Jwts.jwsHeader()
+                .setKeyId('TEST_PUB_KEY_ID'))
+                .signWith(SignatureAlgorithm.RS256, keyPair.privateKey)
+                .compact()
+
+        invalidSignatureIdTokenJwt =  Jwts.builder()
+                .setSubject("00uid4BxXw6I6TV4m0g3")
+                .claim("name", "Joe Coder")
+                .claim("email", "joe.coder@example.com")
+                .claim("preferred_username", "jod.coder@example.com")
+                .setAudience("OOICU812")
+                .setIssuer("http://localhost:9090/oauth2/default")
+                .setIssuedAt(Date.from(now))
+                .setNotBefore(Date.from(now))
+                .setExpiration(Date.from(now.plus(1, ChronoUnit.HOURS)))
+                .setHeader(Jwts.jwsHeader()
+                .setKeyId('TEST_PUB_KEY_ID'))
+                .signWith(SignatureAlgorithm.RS256, invalidKeyPair.privateKey)
+                .compact()
+
+        invalidIssuerIdTokenJwt =  Jwts.builder()
+                .setSubject("00uid4BxXw6I6TV4m0g3")
+                .claim("name", "Joe Coder")
+                .claim("email", "joe.coder@example.com")
+                .claim("preferred_username", "jod.coder@example.com")
+                .setAudience("OOICU812")
+                .setIssuer("http://localhost:9090/oauth2/default_INVALID")
+                .setIssuedAt(Date.from(now))
+                .setNotBefore(Date.from(now))
+                .setExpiration(Date.from(now.plus(1, ChronoUnit.HOURS)))
+                .setHeader(Jwts.jwsHeader()
+                .setKeyId('TEST_PUB_KEY_ID'))
+                .signWith(SignatureAlgorithm.RS256, keyPair.privateKey)
+                .compact()
+
+        invalidNotBeforeIdTokenJwt =  Jwts.builder()
+                .setSubject("00uid4BxXw6I6TV4m0g3")
+                .claim("name", "Joe Coder")
+                .claim("email", "joe.coder@example.com")
+                .claim("preferred_username", "jod.coder@example.com")
+                .setAudience("OOICU812")
+                .setIssuer("http://localhost:9090/oauth2/default")
+                .setIssuedAt(Date.from(now))
+                .setNotBefore(Date.from(now.plus(1, ChronoUnit.HOURS)))
+                .setExpiration(Date.from(now.plus(1, ChronoUnit.HOURS)))
+                .setHeader(Jwts.jwsHeader()
+                .setKeyId('TEST_PUB_KEY_ID'))
+                .signWith(SignatureAlgorithm.RS256, keyPair.privateKey)
+                .compact()
+    }
+
+    Map getBindingMap() {
+        return [
+                accessTokenJwt: accessTokenJwt,
+                pubKeyE: pubKeyE,
+                pubKeyN: pubKeyN,
+                idTokenJwt: idTokenJwt
+        ]
+    }
+
+    void configureHttpMock(WireMockServer wireMockServer) {
+        wireMockServer.stubFor(
+                get("/oauth2/default/.well-known/openid-configuration")
+                        .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBodyFile("discovery.json")
+                        .withTransformers("gstring-template")))
+
+        wireMockServer.stubFor(
+                get(urlPathEqualTo("/oauth2/default/v1/authorize"))
+                        .withQueryParam("client_id", matching(clientId))
+                        .withQueryParam("redirect_uri", matching(Pattern.quote("http://localhost:")+ "\\d+/authorization-code/callback"))
+                        .withQueryParam("response_type", matching("code"))
+                        .withQueryParam("scope", matching("profile email openid"))
+                        .withQueryParam("state", matching(".{6,}"))
+                        .willReturn(aResponse()
+                        .withBody("<html>fake_login_page<html/>")))
+
+        wireMockServer.stubFor(
+                post(urlPathEqualTo("/oauth2/default/v1/token"))
+                        .withHeader("Authorization", equalTo(authHeader))
+                        .withRequestBody(containing("grant_type=authorization_code"))
+                        .withRequestBody(containing("code=TEST_CODE&"))
+                        .withRequestBody(matching(".*"+Pattern.quote("redirect_uri=http%3A%2F%2Flocalhost%3A") + "\\d+" +Pattern.quote("%2Fauthorization-code%2Fcallback") +".*"))
+                        .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json;charset=UTF-8")
+                        .withBodyFile("token.json")
+                        .withTransformer("gstring-template", "idTokenJwt", idTokenJwt)))
+
+        wireMockServer.stubFor(
+                post(urlPathEqualTo("/oauth2/default/v1/token"))
+                        .withHeader("Authorization", equalTo(authHeader))
+                        .withRequestBody(containing("grant_type=authorization_code"))
+                        .withRequestBody(containing("code=TEST_CODE_wrongKeyIdIdTokenJwt&"))
+                        .withRequestBody(matching(".*"+Pattern.quote("redirect_uri=http%3A%2F%2Flocalhost%3A") + "\\d+" +Pattern.quote("%2Fauthorization-code%2Fcallback")  +".*"))
+                        .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json;charset=UTF-8")
+                        .withBodyFile("token.json")
+                        .withTransformer("gstring-template", "idTokenJwt", wrongKeyIdIdTokenJwt)))
+
+        wireMockServer.stubFor(
+                post(urlPathEqualTo("/oauth2/default/v1/token"))
+                        .withHeader("Authorization", equalTo(authHeader))
+                        .withRequestBody(containing("grant_type=authorization_code"))
+                        .withRequestBody(containing("code=TEST_CODE_invalidSignatureIdTokenJwt&"))
+                        .withRequestBody(matching(".*"+Pattern.quote("redirect_uri=http%3A%2F%2Flocalhost%3A") + "\\d+" +Pattern.quote("%2Fauthorization-code%2Fcallback")  +".*"))
+                        .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json;charset=UTF-8")
+                        .withBodyFile("token.json")
+                        .withTransformer("gstring-template", "idTokenJwt", invalidSignatureIdTokenJwt)))
+
+        wireMockServer.stubFor(
+                post(urlPathEqualTo("/oauth2/default/v1/token"))
+                        .withHeader("Authorization", equalTo(authHeader))
+                        .withRequestBody(containing("grant_type=authorization_code"))
+                        .withRequestBody(containing("code=TEST_CODE_invalidIssuerIdTokenJwt&"))
+                        .withRequestBody(matching(".*"+Pattern.quote("redirect_uri=http%3A%2F%2Flocalhost%3A") + "\\d+" +Pattern.quote("%2Fauthorization-code%2Fcallback")  +".*"))
+                        .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json;charset=UTF-8")
+                        .withBodyFile("token.json")
+                        .withTransformer("gstring-template", "idTokenJwt", invalidIssuerIdTokenJwt)))
+
+        wireMockServer.stubFor(
+                post(urlPathEqualTo("/oauth2/default/v1/token"))
+                        .withHeader("Authorization", equalTo(authHeader))
+                        .withRequestBody(containing("grant_type=authorization_code"))
+                        .withRequestBody(containing("code=TEST_CODE_issuedInFutureIdTokenJwt&"))
+                        .withRequestBody(matching(".*"+Pattern.quote("redirect_uri=http%3A%2F%2Flocalhost%3A") + "\\d+" +Pattern.quote("%2Fauthorization-code%2Fcallback")  +".*"))
+                        .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json;charset=UTF-8")
+                        .withBodyFile("token.json")
+                        .withTransformer("gstring-template", "idTokenJwt", issuedInFutureIdTokenJwt)))
+
+        wireMockServer.stubFor(
+                post(urlPathEqualTo("/oauth2/default/v1/token"))
+                        .withHeader("Authorization", equalTo(authHeader))
+                        .withRequestBody(containing("grant_type=authorization_code"))
+                        .withRequestBody(containing("code=TEST_CODE_expiredIdTokenJwt&"))
+                        .withRequestBody(matching(".*"+Pattern.quote("redirect_uri=http%3A%2F%2Flocalhost%3A") + "\\d+" +Pattern.quote("%2Fauthorization-code%2Fcallback")  +".*"))
+                        .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json;charset=UTF-8")
+                        .withBodyFile("token.json")
+                        .withTransformer("gstring-template", "idTokenJwt", expiredIdTokenJwt)))
+
+        wireMockServer.stubFor(
+                post(urlPathEqualTo("/oauth2/default/v1/token"))
+                        .withHeader("Authorization", equalTo(authHeader))
+                        .withRequestBody(containing("grant_type=authorization_code"))
+                        .withRequestBody(containing("code=TEST_CODE_wrongAudienceIdTokenJwt&"))
+                        .withRequestBody(matching(".*"+Pattern.quote("redirect_uri=http%3A%2F%2Flocalhost%3A") + "\\d+" +Pattern.quote("%2Fauthorization-code%2Fcallback")  +".*"))
+                        .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json;charset=UTF-8")
+                        .withBodyFile("token.json")
+                        .withTransformer("gstring-template", "idTokenJwt", wrongAudienceIdTokenJwt)))
+
+        wireMockServer.stubFor(
+                post(urlPathEqualTo("/oauth2/default/v1/token"))
+                        .withHeader("Authorization", equalTo(authHeader))
+                        .withRequestBody(containing("grant_type=authorization_code"))
+                        .withRequestBody(containing("code=TEST_CODE_invalidNotBeforeIdTokenJwt&"))
+                        .withRequestBody(matching(".*"+Pattern.quote("redirect_uri=http%3A%2F%2Flocalhost%3A") + "\\d+" +Pattern.quote("%2Fauthorization-code%2Fcallback")  +".*"))
+                        .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json;charset=UTF-8")
+                        .withBodyFile("token.json")
+                        .withTransformer("gstring-template", "idTokenJwt", invalidNotBeforeIdTokenJwt)))
+
+        wireMockServer.stubFor(
+                get(urlPathEqualTo("/oauth2/default/v1/keys"))
+                        .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json;charset=UTF-8")
+                        .withBodyFile("keys.json")
+                        .withTransformers("gstring-template")))
+
+        wireMockServer.stubFor(
+                get(urlPathEqualTo("/oauth2/default/v1/userinfo"))
+                        .withHeader("Authorization", containing("Bearer ${accessTokenJwt}"))
+                        .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json;charset=UTF-8")
+                        .withBodyFile("userinfo.json")))
+    }
+}
---
+++ src/main/groovy/com/okta/test/mock/tests/OIDCCodeFlowLocalValidationIT.groovy
@@ -0,0 +1,272 @@
+/*
+ * Copyright 2017 Okta, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.okta.test.mock.tests
+
+import com.okta.test.mock.Scenario
+import com.okta.test.mock.application.ApplicationTestRunner
+import io.restassured.http.ContentType
+import io.restassured.response.ExtractableResponse
+import org.hamcrest.Matchers
+import org.testng.annotations.Test
+import java.util.regex.Pattern
+
+import static io.restassured.RestAssured.given
+import static org.hamcrest.Matchers.is
+import static org.hamcrest.text.MatchesPattern.matchesPattern
+import static com.okta.test.mock.scenarios.Scenario.OIDC_CODE_FLOW_LOCAL_VALIDATION
+
+@Scenario(OIDC_CODE_FLOW_LOCAL_VALIDATION)
+class OIDCCodeFlowLocalValidationIT extends ApplicationTestRunner {
+    @Test
+    ExtractableResponse redirectToRemoteLogin() {
+        String expectedRedirect = Pattern.quote(
+                "http://localhost:${doGetMockPort()}/oauth2/default/v1/authorize" +
+                "?scope=profile%20email%20openid" +
+                "&response_type=code" +
+                "&redirect_uri=http%3A%2F%2Flocalhost%3A${applicationPort}%2Fauthorization-code%2Fcallback" +
+                "&state=")+".{36}" +
+                "&client_id=OOICU812"
+
+        return given()
+            .redirects()
+                .follow(false)
+            .accept(ContentType.JSON)
+        .when()
+            .get("http://localhost:${applicationPort}/login")
+        .then()
+            .statusCode(302)
+            .header("Location", matchesPattern(expectedRedirect))
+        .extract()
+    }
+
+    @Test
+    void followRedirect() {
+        given()
+            .accept(ContentType.JSON)
+        .when()
+            .get("http://localhost:${applicationPort}/login")
+        .then()
+            .statusCode(200)
+            .body(Matchers.equalTo("<html>fake_login_page<html/>"))
+    }
+
+    @Test
+    void respondWithCode() {
+        ExtractableResponse response = redirectToRemoteLogin()
+        String redirectUrl = response.header("Location")
+        String state = getState(redirectUrl)
+        String code = "TEST_CODE"
+        String requestUrl = "http://localhost:${applicationPort}/authorization-code/callback?code=${code}&state=${state}"
+
+        given()
+            .accept(ContentType.JSON)
+            .cookies(response.cookies())
+            .redirects()
+                .follow(true)
+        .when()
+            .get(requestUrl)
+        .then()
+            .statusCode(200)
+            .body(Matchers.containsString("Welcome home"))
+        .extract()
+    }
+
+    @Test
+    void wrongStateTest() {
+        ExtractableResponse response = redirectToRemoteLogin()
+        String redirectUrl = response.header("Location")
+        String state = getState(redirectUrl) + "wrong"
+        String code = "TEST_CODE"
+        String requestUrl = "http://localhost:${applicationPort}/authorization-code/callback?code=${code}&state=${state}"
+
+        ExtractableResponse response2 = given()
+            .accept(ContentType.JSON)
+            .cookies(response.cookies())
+            .redirects()
+                .follow(false)
+        .when()
+            .get(requestUrl)
+        .then()
+            .statusCode(401)
+        .extract()
+    }
+
+    @Test
+    void noAuthCodeTest() {
+        ExtractableResponse response = redirectToRemoteLogin()
+        String redirectUrl = response.header("Location")
+        String state = getState(redirectUrl)
+        String requestUrl = "http://localhost:${applicationPort}/authorization-code/callback?state=${state}"
+
+        ExtractableResponse response2 = given()
+            .accept(ContentType.JSON)
+            .cookies(response.cookies())
+            .redirects()
+                .follow(false)
+        .when()
+            .get(requestUrl)
+        .then()
+            .statusCode(401)
+        .extract()
+    }
+
+    @Test
+    void invalidSignatureIdTokenJwtTest() {
+        ExtractableResponse response = redirectToRemoteLogin()
+        String redirectUrl = response.header("Location")
+        String state = getState(redirectUrl)
+        String code = "TEST_CODE_invalidSignatureIdTokenJwt"
+        String requestUrl = "http://localhost:${applicationPort}/authorization-code/callback?code=${code}&state=${state}"
+
+        given()
+            .accept(ContentType.JSON)
+            .cookies(response.cookies())
+            .redirects()
+                .follow(false)
+        .when()
+            .get(requestUrl)
+        .then()
+            .statusCode(401)
+    }
+
+    @Test
+    void wrongKeyIdIdTokenJwtTest() {
+        ExtractableResponse response = redirectToRemoteLogin()
+        String redirectUrl = response.header("Location")
+        String state = getState(redirectUrl)
+        String code = "TEST_CODE_wrongKeyIdIdTokenJwt"
+        String requestUrl = "http://localhost:${applicationPort}/authorization-code/callback?code=${code}&state=${state}"
+
+        given()
+            .accept(ContentType.JSON)
+            .cookies(response.cookies())
+            .redirects()
+                .follow(false)
+        .when()
+            .get(requestUrl)
+        .then()
+            .statusCode(401)
+    }
+
+    @Test
+    void issuedInFutureIdTokenJwtTest() {
+        ExtractableResponse response = redirectToRemoteLogin()
+        String redirectUrl = response.header("Location")
+        String state = getState(redirectUrl)
+        String code = "TEST_CODE_issuedInFutureIdTokenJwt"
+        String requestUrl = "http://localhost:${applicationPort}/authorization-code/callback?code=${code}&state=${state}"
+
+        given()
+            .accept(ContentType.JSON)
+            .cookies(response.cookies())
+            .redirects()
+                .follow(false)
+        .when()
+            .get(requestUrl)
+        .then()
+            .statusCode(401)
+    }
+
+    @Test
+    void expiredIdTokenJwtTest() {
+        ExtractableResponse response = redirectToRemoteLogin()
+        String redirectUrl = response.header("Location")
+        String state = getState(redirectUrl)
+        String code = "TEST_CODE_expiredIdTokenJwt"
+        String requestUrl = "http://localhost:${applicationPort}/authorization-code/callback?code=${code}&state=${state}"
+
+        given()
+            .accept(ContentType.JSON)
+            .cookies(response.cookies())
+            .redirects()
+                .follow(false)
+        .when()
+            .get(requestUrl)
+        .then()
+            .statusCode(401)
+    }
+
+    @Test
+    void wrongAudienceIdTokenJwtTest() {
+        ExtractableResponse response = redirectToRemoteLogin()
+        String redirectUrl = response.header("Location")
+        String state = getState(redirectUrl)
+        String code = "TEST_CODE_wrongAudienceIdTokenJwt"
+        String requestUrl = "http://localhost:${applicationPort}/authorization-code/callback?code=${code}&state=${state}"
+
+        given()
+            .accept(ContentType.JSON)
+            .cookies(response.cookies())
+            .redirects()
+                .follow(false)
+        .when()
+            .get(requestUrl)
+        .then()
+            .statusCode(401)
+    }
+
+    @Test
+    void invalidIssuerIdTokenJwtTest() {
+        ExtractableResponse response = redirectToRemoteLogin()
+        String redirectUrl = response.header("Location")
+        String state = getState(redirectUrl)
+        String code = "TEST_CODE_invalidIssuerIdTokenJwt"
+        String requestUrl = "http://localhost:${applicationPort}/authorization-code/callback?code=${code}&state=${state}"
+
+        given()
+            .accept(ContentType.JSON)
+            .cookies(response.cookies())
+            .redirects()
+                .follow(false)
+        .when()
+            .get(requestUrl)
+        .then()
+            .statusCode(401)
+    }
+
+    @Test
+    void inactiveTokenJwtTest() {
+        ExtractableResponse response = redirectToRemoteLogin()
+        String redirectUrl = response.header("Location")
+        String state = getState(redirectUrl)
+        String code = "TEST_CODE_invalidNotBeforeIdTokenJwt"
+        String requestUrl = "http://localhost:${applicationPort}/authorization-code/callback?code=${code}&state=${state}"
+
+        given()
+            .accept(ContentType.JSON)
+            .cookies(response.cookies())
+            .redirects()
+                .follow(false)
+        .when()
+            .get(requestUrl)
+        .then()
+            .statusCode(401)
+    }
+
+    private String getState(String redirectUrl) {
+        String state
+        String[] params = redirectUrl.split("&")
+        for (String param : params) {
+            String name = param.split("=")[0];
+            String value = param.split("=")[1];
+            if (name.equals("state")) {
+                state = value
+                break
+            }   
+        }
+        return state
+    }
+}
