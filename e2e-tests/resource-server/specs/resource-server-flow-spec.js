07354d59834f8f2d515491b40aec55671776db00
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2018-01-10 09:27:35 -0800

    Addressing comments by Robert and Jordan.
    - Login parameters are passed through env vars. No hard coding in conf.js
    - Check if clicking login changes domain to okta-hosted page/local custom login page

--- e2e-tests/resource-server/specs/resource-server-flow-spec.js
+++ e2e-tests/resource-server/specs/resource-server-flow-spec.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright (c) 2015-2016, Okta, Inc. and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015-2018, Okta, Inc. and/or its affiliates. All rights reserved.
  * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
  *
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
@@ -28,29 +28,13 @@ describe('Resource server Flow', () => {
   });
  
   it('can access resource server messages after login', async () => {
-    let username, password;
-
-    // You can pass username, password for the tests either through env vars or protractor configuration
-    // Env vars takes precedence. If not set, the value in conf.js will be used
-    if (process.env.username) {
-      username = process.env.username;
-    } else {
-      username = browser.params.login.username;
-    }
-
-    if (process.env.password) {
-      password = process.env.password;
-    } else {
-      password = browser.params.login.password;
-    }
-
-    browser.get('http://localhost:8080/');
+    browser.get(browser.params.appRoot);
     loginHomePage.waitForPageLoad();
 
     loginHomePage.clickLoginButton();
     oktaSignInPage.waitForPageLoad();
 
-    await oktaSignInPage.login(username, password);
+    await oktaSignInPage.login(browser.params.login.username, browser.params.login.password);
     authenticatedHomePage.waitForPageLoad();
 
     authenticatedHomePage.viewMessages();
