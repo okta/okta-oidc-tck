bd5a94c81ce66fa0eb4a2074fe7f96ea5373f8b0
Brian Retterer <bretterer@gmail.com>
2018-02-23 16:16:07 -0800

    Fix spaces to 2 spaces

--- e2e-tests/resource-server/specs/okta-resource-server-spec.js
+++ e2e-tests/resource-server/specs/okta-resource-server-spec.js
@@ -15,61 +15,61 @@ const commonUtils = require('../../tools/common-util');
 const request = require('request');
 
 describe('Okta Resource Server',  () => {
-    const appRoot = `http://localhost:${browser.params.appPort}`;
+  const appRoot = `http://localhost:${browser.params.appPort}`;
 
 
-    beforeAll(async () => {
-        this.accessToken = await commonUtils.getAccessToken({
-            ISSUER: process.env.ISSUER,
-            CLIENT_ID: process.env.CLIENT_ID,
-            REDIRECT_URI: process.env.REDIRECT_URI,
-            USERNAME: process.env.USERNAME,
-            PASSWORD: process.env.PASSWORD
-        });
+  beforeAll(async () => {
+    this.accessToken = await commonUtils.getAccessToken({
+      ISSUER: process.env.ISSUER,
+      CLIENT_ID: process.env.CLIENT_ID,
+      REDIRECT_URI: process.env.REDIRECT_URI,
+      USERNAME: process.env.USERNAME,
+      PASSWORD: process.env.PASSWORD
     });
+  });
 
-    beforeEach( () => {
-        browser.ignoreSynchronization = true;
+  beforeEach( () => {
+    browser.ignoreSynchronization = true;
+  });
+
+  it('requires an access token to get the /api/messages data', (done) => {
+
+    request.get(appRoot + "/api/messages", {
+      'auth': {
+        'bearer': this.accessToken
+      },
+        'json': true
+      }, function (error, response, body) {
+        expect(body).not.toBe(null);
+        expect(response.statusCode).toBe(200);
+        expect(Object.keys(body)).toContain('messages');
+        expect(body.messages).not.toBe(null);
+        expect(Object.keys(body.messages[0])).toContain('date');
+        expect(Object.keys(body.messages[0])).toContain('text');
+        done();
     });
+  });
 
-    it('requires an access token to get the /api/messages data', (done) => {
+  it('returns a 401 when trying to access the /api/messages route with an invalid token', (done) => {
+    request.get(appRoot + "/api/messages", {
+      'auth': {
+        'bearer': 'invalid.access.token'
+      },
+      'json': true
+    }, function (error, response, body) {
+      expect(response.statusCode).toBe(401);
 
-        request.get(appRoot + "/api/messages", {
-            'auth': {
-                'bearer': this.accessToken
-            },
-            'json': true
-        }, function (error, response, body) {
-            expect(body).not.toBe(null);
-            expect(response.statusCode).toBe(200);
-            expect(Object.keys(body)).toContain('messages');
-            expect(body.messages).not.toBe(null);
-            expect(Object.keys(body.messages[0])).toContain('date');
-            expect(Object.keys(body.messages[0])).toContain('text');
-            done();
-        });
+      done();
     });
+  });
 
-    it('returns a 401 when trying to access the /api/messages route with an invalid token', (done) => {
-        request.get(appRoot + "/api/messages", {
-            'auth': {
-                'bearer': 'invalid.access.token'
-            },
-            'json': true
-        }, function (error, response, body) {
-            expect(response.statusCode).toBe(401);
+  it('returns a 401 when trying to access the /api/messages route without a token', (done) => {
+    request.get(appRoot + "/api/messages", {
+      'json': true
+    }, function (error, response, body) {
+      expect(response.statusCode).toBe(401);
 
-            done();
-        });
+      done();
     });
-
-    it('returns a 401 when trying to access the /api/messages route without a token', (done) => {
-        request.get(appRoot + "/api/messages", {
-            'json': true
-        }, function (error, response, body) {
-            expect(response.statusCode).toBe(401);
-
-            done();
-        });
-    });
+  });
 });
