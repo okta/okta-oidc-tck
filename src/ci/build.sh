b467f5e771bf6833653a42f688d7ee45b776a8df
Brian Demers <bdemers@apache.org>
2018-09-27 10:57:19 -0400

    Fixing lint issues in shell scripts

--- src/ci/build.sh
+++ src/ci/build.sh
@@ -18,38 +18,39 @@
 set -e
 
 COMMON_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/common.sh"
+# shellcheck source=src/ci/common.sh
 source "${COMMON_SCRIPT}"
 
 cron () {
     echo "Running TRAVIS CRON task"
-    $MVN_CMD dependency-check:aggregate -Powasp
+    ${MVN_CMD} dependency-check:aggregate -Powasp
 }
 
 deploy () {
     echo "Deploying SNAPSHOT build"
-    $MVN_CMD deploy -Pci
+    ${MVN_CMD} deploy -Pci
 }
 
 full_build () {
     echo "Running mvn install"
-    $MVN_CMD install -Pci
+    ${MVN_CMD} install -Pci
 }
 
 no_its_build () {
     echo "Skipping ITs, likely this build is a pull request from a fork"
-    $MVN_CMD install -DskipITs -Pci
+    ${MVN_CMD} install -DskipITs -Pci
 }
 
 # if this build was triggered via a cron job, just scan the dependencies
-if [ "$TRAVIS_EVENT_TYPE" = "cron" ] ; then
+if [ "${TRAVIS_EVENT_TYPE}" = "cron" ] ; then
     cron
 else
     # run 'mvn deploy' if we can
-    if [ "$DEPLOY" = true ] ; then
+    if [ "${DEPLOY}" = true ] ; then
         deploy
     else
         # else try to run the ITs if possible (for someone who has push access to the repo
-        if [ "$RUN_ITS" = true ] ; then
+        if [ "${RUN_ITS}" = true ] ; then
             full_build
         else
             # fall back to running an install and skip the ITs
