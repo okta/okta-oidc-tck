ff42bb17bd752d3c2f26eeea0c0cf7b3f08a75af
Vijet M <vijet.mahabaleshwar@okta.com>
2021-03-17 22:36:16 -0700

    Adds page object for OIE sign-in page

--- e2e-tests/custom-login/specs/custom-login-flow-spec.js
+++ e2e-tests/custom-login/specs/custom-login-flow-spec.js
@@ -13,7 +13,10 @@
 'use strict';
 
 const LoginHomePage = require('../../page-objects/shared/login-home-page');
-const CustomSignInPage = require('../../page-objects/custom-signin-page');
+let OktaSignInPage = require('../../page-objects/okta-signin-page');
+if (process.env.ORG_OIE_ENABLED) {
+  OktaSignInPage = require('../../page-objects/okta-oie-signin-page');
+}
 const AuthenticatedHomePage = require('../../page-objects/shared/authenticated-home-page');
 const ProfilePage = require('../../page-objects/shared/profile-page');
 const MessagesPage = require('../../page-objects/messages-page');
@@ -21,7 +24,7 @@ const url = require('url');
 
 describe('Custom Login Flow', () => {
   const loginHomePage = new LoginHomePage();
-  const customSignInPage = new CustomSignInPage();
+  const customSignInPage = new OktaSignInPage();
   const authenticatedHomePage = new AuthenticatedHomePage();
   const profile = new ProfilePage();
   const messagesPage = new MessagesPage();
